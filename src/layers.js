9e9d43acce635fcac3ca6493926150b8c261cd9a
hienlh <hienlh1298@gmail.com>
2019-10-10 01:14:11 +0700

    Add sprite in layer

--- src/layers.js
+++ src/layers.js
@@ -7,6 +7,12 @@ Game.layers[1].extend = cc.Layer.extend({
 });
 Game.layers[1].start = function (game) {
     var size = cc.director.getWinSize();
+    
+    var sprite = cc.Sprite.create("HelloWorld.png");
+    sprite.setPosition(size.width / 2, size.height / 2);
+    sprite.setScale(0.8);
+    game.addChild(sprite, 0);
+
     var label = cc.LabelTTF.create("Hello World", "Arial", 40);
     label.setPosition(size.width / 2, size.height / 2);
     game.addChild(label, 1);
